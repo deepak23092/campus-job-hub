19f8145ddd4388a56be97db9400adb4488b4f04d
Deepak <deepaklohar974@gmail.com>
2024-03-01 21:49:05 +0530

    backend updated

--- Backend/index.js
+++ Backend/index.js
@@ -7,6 +7,11 @@ require('dotenv').config();
 const app = express();
 app.use(express.json());
 
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.DB_HOST;
+
+app.use(bodyParser.json());
+
 app.use(cors({
     origin: 'https://campus-job-hub.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
@@ -14,17 +19,13 @@ app.use(cors({
     allowedHeaders: 'Content-Type, auth-Token'
 }));
 
-const PORT = process.env.PORT || 3000;
-const HOST = process.env.DB_HOST;
-
-app.use(bodyParser.json());
+app.get("/", async (req, res) => {
+    res.send("server");
+})
 
 app.use('/jobs/', require('./routes/jobRoutes'));
 app.use('/company/', require('./routes/companyRoutes'));
 
-app.get("/", async (req, res) => {
-    res.send("server");
-})
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://${HOST}:${PORT}`);
